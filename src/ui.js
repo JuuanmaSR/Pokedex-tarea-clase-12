a31b75eb619e122f9de67e971fd087eb2433881d
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-04-08 22:39:29 -0300

    modulo-ui
    Este es el modulo donde encontramos todas las funciones que interactúan con lo visual de la pokédex.

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,34 @@
-import { crearListaPokemones } from "./ux.js";
+import { crearListaPokemones,manejarClicks} from "./ux.js";
+ 
+
 export function mostrarListaPokemones(){
     crearListaPokemones();
+}
+
+export function ocultarBotonAnterior() {
+  const $anterior = document.querySelector('#boton-anterior-list');
+  $anterior.className = 'button oculto';
+
+}
+
+export function mostrarBotonAnterior() {
+  const $anterior = document.querySelector('#boton-anterior-list');
+  $anterior.className = 'button';
+}
+
+export function borrarLista() {
+  const liElements = document.querySelectorAll('.list-pokemon__ul li');
+
+  liElements.forEach((li) => {
+    li.remove();
+  });
+}
+
+export function mostrarResultadoPokemon() {
+  const resultadoPokemon = document.querySelector('#resultado-pokemon');
+  resultadoPokemon.className = 'container-pokemon';
+}
+
+export function mostrarResultado (){
+    manejarClicks();
 }
