349953d87a0b85db00ed1f459a53e3061ca1aaaf
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-04-20 16:04:28 -0300

    https-intercept
    Interceptando un request de https con cypress y devolviendo un fixture con los datos que nosotros guardamos para evitar errores externos de terceros, en este caso de la API de pokémon.

--- cypress/integration/pokedex-test.spec.js
+++ cypress/integration/pokedex-test.spec.js
@@ -1,25 +1,33 @@
-describe('Testando pokédex tarea-clase-12',()=> {
-    before(()=>{
-        cy.visit('http://192.168.1.2:8080');
-    });
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-undef */
+/// <reference types="Cypress" />
 
-    it('Testeando existencia de una lista', () => {
-        cy.get(`.list-pokemon__ul`).should(`be.visible`);
-        cy.get(`.list-pokemon__ul li`).should(`have.length`, `20`);
-    });
+describe('Testando pokédex tarea-clase-12', () => {
+  /// esto emula un fetch de tipo XMLhttpRequest en vez del original que usa JS///
 
-    it('Testeando comportamiento adecuado de los botones', () => {
-        cy.get(`#boton-anterior-list`).should(`not.visible`);
-        cy.get(`#boton-siguiente-list`).click();
-        cy.get(`#boton-anterior-list`).should(`be.visible`);
-        cy.get(`#boton-anterior-list`).click();
-        cy.get(`#boton-anterior-list`).should(`not.visible`);
+  before(() => {
+    cy.visit('http://192.168.1.5:8080', {
     });
-    it('Testando visibilidad de los resultados antes de seleccionar elemento de la lista', () => {
-        cy.get(`.mostrar-pokemon`).should(`not.visible`);
-    });
-    it('Seleccionando pokemon y mostrando sus datos', () => {
-        cy.get(`.list-pokemon__ul li`).eq(0).click(); 
-        cy.get(`.mostrar-pokemon`).should(`be.visible`);
-    });
+  });
+
+  it('Testeando existencia de una lista', () => {
+    cy.get('.list-pokemon__ul').should('be.visible');
+    cy.get('.list-pokemon__ul li').should('have.length', '20');
+  });
+
+  it('Testeando comportamiento adecuado de los botones', () => {
+    cy.get('#boton-anterior-list').should('not.visible');
+    cy.get('#boton-siguiente-list').click();
+    cy.get('#boton-anterior-list').should('be.visible');
+    cy.get('#boton-anterior-list').click();
+    cy.get('#boton-anterior-list').should('not.visible');
+  });
+  it('Testando visibilidad de los resultados antes de seleccionar elemento de la lista', () => {
+    cy.get('.mostrar-pokemon').should('not.visible');
+  });
+  it('Seleccionando pokemon y mostrando sus datos', () => {
+    cy.intercept('https://pokeapi.co/api/v2/pokemon/charizard', { fixture: 'charizard.json' });
+    cy.get('.list-pokemon__ul a li').eq(5).click();
+    cy.get('.mostrar-pokemon').should('be.visible');
+  });
 });
