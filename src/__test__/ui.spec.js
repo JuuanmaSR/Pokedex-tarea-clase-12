3f8cb10c4f0101abf0a8cd1f7df52f2e01e21d38
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-04-26 14:41:25 -0300

    test-ui

--- src/__test__/ui.spec.js
+++ src/__test__/ui.spec.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 /* eslint-disable import/extensions */
 
-import { borrarLista, mostrarBotonAnterior, ocultarBotonAnterior } from '../ui.js';
+import {
+  borrarLista, mostrarBotonAnterior, mostrarResultadoPokemon, ocultarBotonAnterior,
+} from '../ui.js';
 
 test('comprobar que se oculte un boton', () => {
   document.body.innerHTML = '<button id="boton-anterior-list">Anterior</button>';
@@ -10,13 +12,20 @@ test('comprobar que se oculte un boton', () => {
 });
 
 test('comprobar que se muestre un boton', () => {
-  document.body.innerHTML = '<button class="button-oculto" id="boton-anterior-list">Anterior</button>';
+  document.body.innerHTML = '<button class="button-oculto" id="boton-anterior-list">Anterior</button><button class="button" id="boton-anterior-list-2">Anterior motrado</button>';
   mostrarBotonAnterior();
   expect(document.querySelector('#boton-anterior-list').className).toContain('button');
+  expect(document.querySelector('#boton-anterior-list-2').className).toContain('button');
 });
 
 test('comprobar que una lista se borre', () => {
   document.body.innerHTML = '<ul class="list-pokemon__ul"><li>hola</li><li>chau</li><li>adios</li></ul>';
   borrarLista();
   expect(document.querySelectorAll('.list-pokemon__ul li').length).toBe(0);
+});
+
+test('comprobar que se muestre un resultado', () => {
+  document.body.innerHTML = '<div id="resultado-pokemon"></div>';
+  mostrarResultadoPokemon();
+  expect(document.querySelector('#resultado-pokemon').className).toContain('container-pokemon');
 });
